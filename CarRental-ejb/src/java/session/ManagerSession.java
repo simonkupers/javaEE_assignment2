15819436b98082e996fec7efdfe133456e8041c6
simon <simon@DESKTOP-775LSF5>
2018-11-13 17:15:41 +0100

    add Cars, Companies and CarTypes

--- CarRental-ejb/src/java/session/ManagerSession.java
+++ CarRental-ejb/src/java/session/ManagerSession.java
@@ -1,10 +1,20 @@
 package session;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static javafx.scene.input.KeyCode.T;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -12,21 +22,21 @@ import rental.Car;
 import rental.CarRentalCompany;
 import rental.CarType;
 import rental.RentalStore;
+import static rental.RentalStore.loadRental;
 import rental.Reservation;
 
 @Stateless
 public class ManagerSession implements ManagerSessionRemote {
-    
+
     @PersistenceContext
     private EntityManager em;
 
-    
     @Override
     public Set<CarType> getCarTypes(String company) {
         try {
             return new HashSet<CarType>((Collection<CarType>) em.createNamedQuery("CarType.findByCompany")
-                .setParameter("company", company)
-                .getResultList());//RentalStore.getRental(company).getAllTypes());
+                    .setParameter("company", company)
+                    .getResultList());//RentalStore.getRental(company).getAllTypes());
 
         } catch (IllegalArgumentException ex) {
             Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
@@ -39,7 +49,7 @@ public class ManagerSession implements ManagerSessionRemote {
         Set<Integer> out = new HashSet<Integer>();
         try {
             CarRentalCompany crc = em.find(CarRentalCompany.class, company);
-            for(Car c: crc.getCars(type)){
+            for (Car c : crc.getCars(type)) {
                 out.add(c.getId());
             }
         } catch (IllegalArgumentException ex) {
@@ -64,8 +74,8 @@ public class ManagerSession implements ManagerSessionRemote {
     public int getNumberOfReservations(String company, String type) {
         Set<Reservation> out = new HashSet<Reservation>();
         try {
-           CarRentalCompany crc = em.find(CarRentalCompany.class, company);
-            for(Car c: crc.getCars(type)){
+            CarRentalCompany crc = em.find(CarRentalCompany.class, company);
+            for (Car c : crc.getCars(type)) {
                 out.addAll(c.getReservations());
             }
         } catch (IllegalArgumentException ex) {
@@ -73,6 +83,76 @@ public class ManagerSession implements ManagerSessionRemote {
             return 0;
         }
         return out.size();
+    }
+
+    public void addCarRentalCompany(String datafile) {
+        try {
+            CrcData data = loadData(datafile);
+            CarRentalCompany company = new CarRentalCompany(data.name, data.regions, data.cars);
+            em.persist(company);
+            Logger.getLogger(ManagerSession.class.getName()).log(Level.INFO, "Loaded {0} from file {1}", new Object[]{data.name, datafile});
+        } catch (NumberFormatException ex) {
+            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, "bad file", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ManagerSession.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void addCar(int uid, CarType type) {
+        Car car = new Car(uid, type);
+        em.persist(car);
+    }
+
+    public void addCarType(CarType type) {
+        em.persist(type);
+    }
+
+    public static CrcData loadData(String datafile)
+            throws NumberFormatException, IOException {
+
+        CrcData out = new CrcData();
+        StringTokenizer csvReader;
+        int nextuid = 0;
+
+        //open file from jar
+        BufferedReader in = new BufferedReader(new InputStreamReader(RentalStore.class.getClassLoader().getResourceAsStream(datafile)));
+
+        try {
+            while (in.ready()) {
+                String line = in.readLine();
+
+                if (line.startsWith("#")) {
+                    // comment -> skip					
+                } else if (line.startsWith("-")) {
+                    csvReader = new StringTokenizer(line.substring(1), ",");
+                    out.name = csvReader.nextToken();
+                    out.regions = Arrays.asList(csvReader.nextToken().split(":"));
+                } else {
+                    csvReader = new StringTokenizer(line, ",");
+                    //create new car type from first 5 fields
+                    CarType type = new CarType(csvReader.nextToken(),
+                            Integer.parseInt(csvReader.nextToken()),
+                            Float.parseFloat(csvReader.nextToken()),
+                            Double.parseDouble(csvReader.nextToken()),
+                            Boolean.parseBoolean(csvReader.nextToken()));
+                    //create N new cars with given type, where N is the 5th field
+                    for (int i = Integer.parseInt(csvReader.nextToken()); i > 0; i--) {
+                        out.cars.add(new Car(nextuid++, type));
+                    }
+                }
+            }
+        } finally {
+            in.close();
+        }
+
+        return out;
+    }
+
+    static class CrcData {
+
+        public List<Car> cars = new LinkedList<Car>();
+        public String name;
+        public List<String> regions = new LinkedList<String>();
     }
 
 }
